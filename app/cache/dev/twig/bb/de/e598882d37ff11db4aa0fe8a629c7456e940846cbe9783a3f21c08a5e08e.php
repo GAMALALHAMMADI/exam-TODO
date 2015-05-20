50e523cd092639a5d691f4dc532dd0f76a4d77b0
YAHYAAhmed <ahmedalwisy@gmail.com>
2015-05-20 23:43:33 +0100

    first commit

---
+++ app/cache/dev/twig/bb/de/e598882d37ff11db4aa0fe8a629c7456e940846cbe9783a3f21c08a5e08e.php
@@ -0,0 +1,89 @@
+<?php
+
+/* ToDoBundle:Tache:new.html.twig */
+class __TwigTemplate_bbdee598882d37ff11db4aa0fe8a629c7456e940846cbe9783a3f21c08a5e08e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        try {
+            $this->parent = $this->env->loadTemplate("::base.html.twig");
+        } catch (Twig_Error_Loader $e) {
+            $e->setTemplateFile($this->getTemplateName());
+            $e->setTemplateLine(1);
+
+            throw $e;
+        }
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "<h1>TODO.COM</h1>
+
+    ";
+        // line 6
+        echo         $this->env->getExtension('form')->renderer->renderBlock((isset($context["form"]) ? $context["form"] : $this->getContext($context, "form")), 'form');
+        echo "
+
+        <ul class=\"record_actions\">
+    <li>
+        <a href=\"";
+        // line 10
+        echo $this->env->getExtension('routing')->getPath("tache");
+        echo "\">
+            Retour
+        </a>
+    </li>
+</ul>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <i> <h6 align=\"center\">  Email: jamalr615@gmail.com //Copyright@Alhammadi Gamal</h6></i>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "ToDoBundle:Tache:new.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  50 => 10,  43 => 6,  39 => 4,  36 => 3,  11 => 1,);
+    }
+}
